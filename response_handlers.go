dda1d3c232adf91665c7aefce6b16f1ba569d2b0
mambo-dev <mambo.michael.22@gmail.com>
2025-04-06 22:07:38 +0300

    fix security issues

--- response_handlers.go
+++ response_handlers.go
@@ -46,7 +46,10 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	w.Write(dat)
+
+	if _, err := w.Write(dat); err != nil {
+		log.Printf("Failed to write response: %v", err)
+	}
 }
 
 func generateValidationError(err error) map[string]string {
