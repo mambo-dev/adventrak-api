ddccbf7600b8c7451b12356cd2478caddb6e2efa
mambo-dev <mambo.michael.22@gmail.com>
2025-04-11 20:47:24 +0300

    stop barebones

---
+++ stop_handler.go
@@ -0,0 +1,23 @@
+package main
+
+import "net/http"
+
+func (cfg apiConfig) handlerGetStops(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (cfg apiConfig) handlerGetStop(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (cfg apiConfig) handlerCreateStop(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (cfg apiConfig) handlerUpdateStop(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (cfg apiConfig) handlerDeleteStop(w http.ResponseWriter, r *http.Request) {
+
+}
