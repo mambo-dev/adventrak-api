325fb1e728f5d13bbf329295fbacdb40696acd47
mambo-dev <mambo.michael.22@gmail.com>
2025-04-22 15:49:11 +0300

    reset handler to delete all photos

--- admin_handlers.go
+++ admin_handlers.go
@@ -7,12 +7,21 @@ import (
 
 func (cfg apiConfig) resetDatabase(w http.ResponseWriter, r *http.Request) {
 
-	err := cfg.db.DeleteUsers(r.Context())
+	dirs, err := os.ReadDir(cfg.assetsRoot)
 
-	if err := os.RemoveAll(cfg.assetsRoot); os.IsNotExist(err) {
-		respondWithError(w, http.StatusNotModified, "Failed to delete user assets and the assets directory.", err, false)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Something went wrong", err, false)
 		return
 	}
+
+	for _, dir := range dirs {
+		if err := os.Remove(dir.Name()); err != nil {
+			respondWithError(w, http.StatusNotModified, "Failed to delete user assets and the assets directory.", err, false)
+			return
+		}
+	}
+
+	err = cfg.db.DeleteUsers(r.Context())
 
 	if err != nil {
 		respondWithError(w, http.StatusNotModified, "Failed to delete all users and their data", err, false)
